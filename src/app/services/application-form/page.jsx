ac0b6a23971ec8a9aec471ff638e683b797c5931
MIGSNOLIMIT <migsnolimit26@gmail.com>
2025-11-19 03:20:31 +0800

    modified:   src/app/services/application-form/page.jsx

--- src/app/services/application-form/page.jsx
+++ src/app/services/application-form/page.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 import { useState, useRef } from "react";
 import { createClient } from "@supabase/supabase-js";
-import Image from "next/image";
+
 
 export default function ApplicationFormPage({ searchParams, requirements = [] }) {
   const submitted = searchParams?.submitted === "1";
